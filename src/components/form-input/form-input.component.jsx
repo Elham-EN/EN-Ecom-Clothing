4aef70f8bf6aa96ff1411de5e82a28b0c8e336b3
Elham-EN <ilhamrezaie@gmail.com>
2021-01-02 23:18:15 +1100

    Added Sign component with sass style

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./form-input.styles.scss";
+
+const FormInput = ({ handleChange, label, ...restOfProps }) => (
+  <div className="group">
+    <input className="form-input" onChange={handleChange} {...restOfProps} />
+    {label ? (
+      <label
+        className={`${
+          restOfProps.value.length ? "shrink" : ""
+        } form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
+
+export default FormInput;
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import FormInput from "../form-input/form-input.component";
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an accound</h2>
+        <span>Sigin in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            label="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            required
+          />
+
+          <FormInput
+            type="password"
+            name="password"
+            label="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+          />
+
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
